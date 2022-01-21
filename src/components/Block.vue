222519aef8a1583846d22f4b18c1a9f8c8928f17
Bakhodur <bakhodur.nazriev@gmail.com>
2022-01-22 01:08:08 +0500

    Create Block and Result component

---
+++ src/components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="block">
+    click me
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Block"
+}
+</script>
+
+<style scoped>
+.block {
+  width: 350px;
+  border-radius: 10px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
